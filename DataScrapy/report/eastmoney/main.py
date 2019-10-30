a47d425922f3a3d7975fafe33ee23ddc4e1836f9
KouenSoftWare <jobkouen@outlook.com>
2019-10-30 22:02:19 +0800

    commit

---
+++ DataScrapy/report/eastmoney/main.py
@@ -0,0 +1,40 @@
+"""
+主要指标：http://f10.eastmoney.com/NewFinanceAnalysis/MainTargetAjax?type=0&code=SH600696
+    type 0, 1, 2分别代表按照报告期、年度、单季度
+资产负载表： http://f10.eastmoney.com/NewFinanceAnalysis/zcfzbAjax?companyType=4&reportDateType=0&reportType=1&endDate=&code=SH600696
+    reportDateType: 0,1 报告期和年度
+    companyType: 1 券商， 2 保险， 3 银行， 4 企业
+    reportType:
+                <li class="first current" reportdatetype="0" reporttype="1">
+                    <span>按报告期</span>
+                </li>
+                <li reportdatetype="1" reporttype="1">
+                    <span>
+                        按年度
+                    </span>
+                </li>
+                <li reportdatetype="0" reporttype="2" style="display:none">
+                    <span>
+                        按单季度
+                    </span>
+                </li>
+                <li reportdatetype="0" reporttype="1">
+                    <span>
+                        报告期同比
+                    </span>
+                </li>
+                <li reportdatetype="1" reporttype="1">
+                    <span>
+                        年度同比
+                    </span>
+                </li>
+                <li reportdatetype="0" reporttype="2" style="display:none">
+                    <span>
+                        单季度同比
+                    </span>
+                </li>
+利润表: http://f10.eastmoney.com/NewFinanceAnalysis/lrbAjax?companyType=4&reportDateType=0&reportType=1&endDate=&code=SH600696
+现金流量表：http://f10.eastmoney.com/NewFinanceAnalysis/xjllbAjax?companyType=4&reportDateType=0&reportType=1&endDate=&code=SH600696
+百分比报表：http://f10.eastmoney.com/NewFinanceAnalysis/PercentAjax?code=SH600696&ctype=4&type=0
+经营分析：http://f10.eastmoney.com/BusinessAnalysis/BusinessAnalysisAjax?code=SH600696
+"""
